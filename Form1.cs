a03fb7232f7acf837e4f3fa7f3d2d01f04b39cea
Jeremy Fabian <118284208+LkyFabian@users.noreply.github.com>
2024-05-13 01:19:19 +0700

    Pengumpulan filenya disini ya kak

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Tp12_1302220143
+{
+    public partial class Form1 : Form
+    {
+        public string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+                return "Negatif";
+            else if (a > 0)
+                return "Positif";
+            else
+                return "Nol";
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int input))
+            {
+                string hasil = CariTandaBilangan(input);
+                label1.Text = $"Hasil pangkat: {hasil}"; // Menggunakan label1.Text, bukan label1_Click.Text
+            }
+            else
+            {
+                MessageBox.Show("Masukkan angka yang valid.");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ UnitTestProject/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tp12_1302220143.Form1;
+
+
+
+namespace UnitTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [DataRow(-5, "Negatif")] // Input negatif
+        [DataRow(5, "Positif")]  // Input positif
+        [DataRow(0, "Nol")]      // Input nol
+        public void CariTandaBilangan_ValidInput_ReturnsExpected(int input, string expectedOutput)
+        {
+            // Arrange
+            Form1 form = new Form1 ();
+
+            // Act
+            string actualOutput = form.CariTandaBilangan(input);
+
+            // Assert
+            Assert.AreEqual(expectedOutput, actualOutput);
+        }
+    }
+}
